160e3fe4ef294fd9912c2908a5f4ea62b43541e1
pyadav6 <pyadav6@stevens.edu>
2023-09-27 19:39:36 -0400

    Adding my brand function to this repo

--- TestTriangle.py
+++ TestTriangle.py
@@ -8,12 +8,13 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 """
 
 import unittest
-
+from brand import my_brand
 from Triangle import classifyTriangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
-
+my_brand_here = False
+my_brand("HW 02a - Testing a legacy program and reporting on testing results")
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
@@ -26,7 +27,25 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
+    def testIsoscelesTriangleA(self): 
+        self.assertEqual(classifyTriangle(4,4,5),'Isosceles','4,4,5 is a Isosceles triangle')
+
+    def testScaleneTriangleA(self): 
+        self.assertEqual(classifyTriangle(6,7,5),'Scalene','4,4,5 is a Scalene triangle')
+
+    def testSideLowerLimitTriangleA(self): 
+        self.assertEqual(classifyTriangle(4,0,-5),'InvalidInput','4,0,5 is a InvalidInput')
+
+    def testSideUpperLimitTriangleA(self): 
+        self.assertEqual(classifyTriangle(201,4,5),'InvalidInput','201,4,5 is a InvalidInput')
+
+    def testFormationTriangleA(self): 
+        self.assertEqual(classifyTriangle(2,3,6),'NotATriangle','2,3,6 is a NotATriangle')
+
 if __name__ == '__main__':
     print('Running unit tests')
-    unittest.main()
+    unittest.main(exit=False)
+
+if not my_brand_here:
+    my_brand("HW 02a - Testing a legacy program and reporting on testing results")
 
